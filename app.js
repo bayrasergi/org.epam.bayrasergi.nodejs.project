bfb44db3da3e14a81e51c8a25a02d00e500a02f3
bayrase3 <sergey.bayramov@novartis.com>
2019-01-15 12:40:55 +0300

    REP-7: add base auth

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const express = require('express');
+const passport = require('./auth/auth');
+const app = express();
+
+app.use(passport.initialize());
+app.use(passport.session());
