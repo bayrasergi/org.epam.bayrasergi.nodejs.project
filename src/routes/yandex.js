37639735331626b5a17988b7dbf031a1855b7e47
bayrase3 <sergey.bayramov@novartis.com>
2019-01-17 19:10:19 +0300

    REP-8: add correct token

--- src/routes/yandex.js
+++ src/routes/yandex.js
@@ -1,45 +1,49 @@
 const axios = require('axios');
+const qs = require('qs');
 const express = require('express');
+const YandexDisk = require('yandex-disk').YandexDisk;
+const config = require('../config/main');
+
+let token = null;
 
 const yandexRoute = express.Router();
 
-// TODO: extract client_id
-
 yandexRoute.get('/code', (req, res, next) => {
-    // axios.get('https://oauth.yandex.ru/authorize',
-    // {
-    //     params:{
-    //         response_type: 'code',
-    //         client_id: '4904eca87dcc4a119d7e42d828783110'
-    //     }
-    // });
     res.redirect('https://oauth.yandex.ru/authorize?response_type=code&client_id=4904eca87dcc4a119d7e42d828783110');
 });
 
 yandexRoute.get('/token', (req, res, next) => {
-    let code = req.params.code;
-    // if (!code) {
-    //     res.redirect('https://oauth.yandex.ru/authorize?response_type=code&client_id=4904eca87dcc4a119d7e42d828783110');
-    // }
-    axios.post('https://oauth.yandex.ru/token', {
+    const body = qs.stringify({
         grant_type: 'authorization_code',
-        code: code,
+        code: req.query.code,
         client_id: '4904eca87dcc4a119d7e42d828783110',
         client_secret: 'c1f6cfc9608444fc9776460420093c93'
-    },
-    {
-        headers: { 
-            'cache-control': 'no-cache',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/json'
-        },
-    })
-    .then((response) => {
-        res.send(response.body);
-    })
-    .catch((error) => {
-        console.error(error.data);
     });
+    const header = {
+        headers : {
+            'content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+        }
+    }
+    axios.post(
+        'https://oauth.yandex.ru/token',
+        body, 
+        header)
+    .then(response => {
+        token = response.data.access_token;
+        next();
+    })
+    .catch(err => {
+        console.error(err);
+        return Promise.reject(err);
+    });
+});
+
+yandexRoute.get('/disk', (req, res, next) => {
+    if (token === null) {
+        res.redirect('https://oauth.yandex.ru/authorize?response_type=code&client_id=4904eca87dcc4a119d7e42d828783110');
+    }
+    console.log(token);
+    res.send({"text": "works good!"});
 });
 
 module.exports = yandexRoute;
